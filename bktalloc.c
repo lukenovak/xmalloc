e3a059fef1ae006569bd218cb81752e06ccb5627
Eli <elijahsteres@gmail.com>
2019-03-24 15:19:11 -0400

    continuing to not test things, but here's the 'finished' thread-safe bucket allocator

--- bktalloc.c
+++ bktalloc.c
@@ -1,10 +1,23 @@
 //TODO: map a page and make the requisite number of arenas
 //for now, I'm assuming they are in a global bktarena* arenas
-__thread int preferred_arena;
+
+void make_arenas (void) __attribute__ ((constructor));
+
+__thread int preferred_arena = 0;
+bktarena* arenas;
+int numarenas;
+
+void make_arenas (void) {
+    numarenas = get_nprocs_conf() * 4;
+    arenas = mmap(NULL, numarenas*sizeof(bktarena), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    for (int i = 0; i < numarenas; i++) {
+        make_bktalloc(&arenas[i]);
+    }
+}
 
 void takelock() {
     if (pthread_mutex_trylock(arenas[preferred_arena]->mutex) == EBUSY) {
-        preferred_arena ++;
+        preferred_arena = (preferred_arena + 1) % numarenas;
         pthread_mutex_lock(arenas[preferred_arena]->mutex);
     }
 }
